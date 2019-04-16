a408ee08b4c920b8316f910c845b94b235383921
tw5033 <tw5033@gmail.com>
2019-04-16 01:15:12 +0000

    added listener for hovering over tweets

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,3 +3,8 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+$(document).ready(function() {
+  $('.tweets').on("mouseover", function() {
+    $('.tweets').css('opacity', '1');
+  });
+});
