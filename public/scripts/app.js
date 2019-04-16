071ac1d144c6357e72583ccc6d6d532519d9774e
tw5033 <tw5033@gmail.com>
2019-04-16 21:27:09 +0000

    finished rendering function to display tweets form data

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,21 +4,53 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(function() {
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
+  // Fake data taken from tweets.json
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": {
+          "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+          "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+          "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+        },
+        "handle": "@SirIsaac"
       },
       "content": {
         "text": "If I have seen further it is by standing on the shoulders of giants"
       },
       "created_at": 1461116232227
-  }
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": {
+          "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+          "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+          "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+        },
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    },
+    {
+      "user": {
+        "name": "Johann von Goethe",
+        "avatars": {
+          "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+          "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+          "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+        },
+        "handle": "@johann49"
+      },
+      "content": {
+        "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+      },
+      "created_at": 1461113796368
+    }
+  ];
 
   const createTweetElement = function(data) {
     // create all the html tags
@@ -32,8 +64,8 @@ $(document).ready(function() {
     const footer = $("<footer>");
     const p = $("<p>");
     const iOne = $("<i>").addClass("fas fa-heart");
-    const iTwo = $("<i>").addClass("fas fa-flag");
-    const iThree = $("<i>").addClass("fas fa-retweet");
+    const iTwo = $("<i>").addClass("fas fa-retweet");
+    const iThree = $("<i>").addClass("fas fa-flag");
 
     //setting data into html tags
     img.attr("src", data.user.avatars.large);
@@ -58,9 +90,13 @@ $(document).ready(function() {
     return section;
   }
 
-  var $tweet = createTweetElement(tweetData);
+  const renderTweets = function(tweets) {
+    tweets.forEach((data) => {
+      let tweet = createTweetElement(data);
+      $(".container").append(tweet);
+    });
+  }
 
-  // Test / driver code (temporary)
-  console.log($tweet); // to see what it looks like
-  $('.container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  renderTweets(data);
+
 });
